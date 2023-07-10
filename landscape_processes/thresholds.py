figure out units, fixes

landscape_processes/thresholds.py
@@ -1,34 +1,32 @@
 import math
 import numpy as np
 
-def 
+def compute_saturated_raster(M, q, T):
+    with xarray.set_options(keep_attrs=True):
+        sat = (T/q) * M
+    return sat
 
-def compute_saturated_raster(slope, precipitation, transmissivity):
-    return slope * (transmissivity / precipitation)
+def compute_channelization_raster(M, q, T, k, v, g, pw, tau):
+    sat = compute_saturated_raster(M, q, T)
+    
+    with xarray.set_options(keep_attrs=True):
+        # 2tau^3
+        numerator = 2 * np.power(tau, 3) 
 
-"""
-def compute_channelization_raster(slope, precipitation, transmissivity, k, v, g, pw, tau):
-   saturated = compute_saturated_raster(slope, precipitation, transmissivity)
+        # pw^3 * kvg^2 * M^2 * q
+        denominator = np.power(pw,3) * k * v * np.power(g,2) * np.power(M,2) * q
 
-   numerator = 2 * np.power(transmissivity, 3)
-   denominator = np.power(pw, 3) * k * v * np.power(g, 2) * np.power(slope, 2) * precipitation
+    return numerator / denominator + sat
 
-   channelization = numerator / denominator + saturated
-   return channelization
+def compute_landsliding_raster(M, q, T, k, v, g, pw, tau):
+    sat = compute_saturated_raster(slope, precip, T)
 
-def compute_landsliding_raster(slope, precipitation, transmissivity, bulk_density, pw, psi, tau):
-   saturated = compute_saturated_raster(slope, precipitation, transmissivity)
-   ps_over_pw = ps / pw
-   middle = 1 - math.tan(slope) / math.tan(psi)
+    ps_over_pw = pb/pw
+    middle = 1 - (math.tan(M) / math.tan(psi))
+    return ps_over_pw * middle * sat
 
-   landsliding = ps_over_pw * middle * saturated
-   return landsliding
+def unconditionally_stable(M, psi):
+    return math.tan(M) > math.tan(psi)
 
-def compute_stability(theta, psi):
-    if math.tan(theta) < (0.5 * math.tan(psi)):
-        return True
-    elif math.tan(theta) > math.tan(psi):
-        return False
-    else:
-        ValueError("tan(psi) >= tan(theta) >= 0.5tan(psi)")
-"""
+def unconditionally_unstable(M, psi):
+    return math.tan(M) < (0.5 * math.tan(psi))